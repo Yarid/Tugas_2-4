28b1597605d415528460fc756613362822bfdfb1
yarid <yaridalasyarisolehudin@gmail.com>
2021-11-15 17:13:29 +0700

    menambahakan tugas 2-4

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,9 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya Yarid Al Asyari Saya tinggal di daerah tasikmalaya olah raga yang saya sukai adalah badbinton";
+    }
+}
---
+++ application/views/v_about.php
@@ -0,0 +1,23 @@
+<section>
+ <h1><?php echo $judul ?></h1>
+ <h4>Nama</h4>
+ <ul type="disc">
+ <li>Nama Depan : Yarid</li>
+ <li>Nama Belakang : Al Asyari</li>
+ </ul>
+ <br>
+ <h4>Alamat</h4>
+ <ul type="none">
+ <li> Tasikmalaya</li>
+ </ul>
+
+ <h4>Tempat Lahir</h4>
+ <ul type="none">
+ <li>Tasikmalaya</li>
+ </ul>
+ <h4>Olah Raga Favorit</h4>
+ <ul type="square">
+ <li>badbinton</li>
+ <li>Senam</li>
+ </ul>
+</section>
